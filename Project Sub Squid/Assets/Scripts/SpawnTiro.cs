45995ece1d6907cc56cbafb04e3a9f0c792ecd8f
AndreZrem <80982199+AndreZrem@users.noreply.github.com>
2021-08-24 12:08:03 -0300

    Tiro

---
+++ Project Sub Squid/Assets/Scripts/SpawnTiro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTiro : MonoBehaviour
+{
+
+    public Transform bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(bullet);
+        }
+    }
+}

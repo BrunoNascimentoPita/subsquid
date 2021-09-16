4028f3550f4c8ece2c6a97b46245df7154babfc0
AndreZrem <80982199+AndreZrem@users.noreply.github.com>
2021-09-16 11:16:59 -0300

    SpanwPowerUps feito
    batata

--- Project Sub Squid/Assets/Scripts/SpawnEnemy.cs
+++ Project Sub Squid/Assets/Scripts/SpawnEnemy.cs
@@ -15,12 +15,22 @@ public class SpawnEnemy : MonoBehaviour
     public float tempoSpawnES;
     public Transform[] pontosdeSpawnES;
 
+    // spawn PowerUps
+
+    public GameObject[] spawnPW;
+    int randomPW;
+    public float spawntime;
+    public float spawndelay;
+
+    public Transform[] pontosdeSpawnPW;
+
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("StartSpawn", tempoSpawn, tempoSpawn);
         InvokeRepeating("StartSpawnES", tempoSpawnES, tempoSpawnES);
+        InvokeRepeating("SpawnRandomPW", spawntime, spawndelay);
     }
 
     // Update is called once per frame
@@ -40,6 +50,15 @@ public class SpawnEnemy : MonoBehaviour
     {
           int PontosSpawnIndex = Random.Range(0, pontosdeSpawn.Length);
           Instantiate(enemy, pontosdeSpawn[PontosSpawnIndex].position, Quaternion.identity);
+
+    }
+
+    void SpawnRandomPW()
+    { 
+            randomPW= Random.Range(0, spawnPW.Length);
+            int PontosSpawnIndexPW = Random.Range(0, pontosdeSpawnPW.Length);
+            Instantiate(spawnPW[randomPW], pontosdeSpawnPW[PontosSpawnIndexPW].position, Quaternion.identity);
+            //Instantiate(spawnPW[randomPW],transform.position, transform.rotation);
 
     }
 

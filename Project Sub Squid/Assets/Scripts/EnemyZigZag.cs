eb2dd03fda0ab4cdf68ae83d90875a3b62a6d1f5
AndreZrem <80982199+AndreZrem@users.noreply.github.com>
2021-09-21 19:50:39 -0300

    EnemyGroupLife
    agr pode ser destruído

--- Project Sub Squid/Assets/Scripts/EnemyZigZag.cs
+++ Project Sub Squid/Assets/Scripts/EnemyZigZag.cs
@@ -7,16 +7,28 @@ public class EnemyZigZag : MonoBehaviour
 
     public float moveSpeed = 5;
 
+    public float vidaEnemyGroup = 5;
+    public float danoEnemyGroup = 2;
+
+    //public Rigidbody enemyG;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //enemyG = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+        if(vidaEnemyGroup <= 0)
+        {
+            GameController.instance.SetScore(10);
+            Destroy(gameObject);
+        }
     }
 
     private void FixedUpdate()
@@ -31,5 +43,40 @@ public class EnemyZigZag : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Tiro")
+        {
+            Debug.Log("levou um tiro do player");
+            vidaEnemyGroup = vidaEnemyGroup - 3;
+        }
+
+        if (other.gameObject.tag == "TiroDuplo")
+        {
+            Debug.Log("levou um tiro do player");
+            vidaEnemyGroup = vidaEnemyGroup - 2;
+        }
+
+        if (other.gameObject.tag == "TiroTriplo")
+        {
+            Debug.Log("levou um tiro do player");
+            vidaEnemyGroup = vidaEnemyGroup - 1;
+        }
+
+        if (other.gameObject.tag == "TiroPesado")
+        {
+            Debug.Log("levou um tiro do Inimigo");
+            vidaEnemyGroup = vidaEnemyGroup - 5;
+            
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("chocou com o player");
+            Destroy(this.gameObject, 0.1f);
+        }
+
     }
 }

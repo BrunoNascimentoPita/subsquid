18771ea317eef270230e8a9fe6ba3575e67ec5c0
AndreZrem <80982199+AndreZrem@users.noreply.github.com>
2021-09-26 23:52:38 -0300

    resolvidoporhora
    ainda não resolvido por completo por que não serve dessa maneira vai prejudicar futuramente então tem que mudar

--- Project Sub Squid/Assets/Scripts/Tiro.cs
+++ Project Sub Squid/Assets/Scripts/Tiro.cs
@@ -10,7 +10,7 @@ public class Tiro : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        FindObjectOfType<Audio_menager>().Play("tiro");
     }
 
     // Update is called once per frame

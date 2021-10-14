6f6b50bcc950951195fb0c0c9f9e156c63aa72ed
Lohansampaio <73318105+Lohansampaio@users.noreply.github.com>
2021-10-14 01:04:49 -0300

    salvando

--- Project Sub Squid/Assets/_Scripts/TiroLaser.cs
+++ Project Sub Squid/Assets/_Scripts/TiroLaser.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class TiroLaser : MonoBehaviour
 {
+
+    public int gunDamage = 1;
+    public float fireRate = 25f;
+    public float weaponRange = 50;
+    public float hitForce = 100;
+    public Transform gunEnd;
+
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.8f);
+    private LineRenderer laserLine;
+    private float nextFire;
    
-    public float speedTiro = 10f;
-   
+  
+
+
+
+    private void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+      
+    }
 
     void Update()
     {
-        transform.position += Vector3.right * speedTiro * Time.deltaTime;
-        Destroy(this.gameObject, 5.0f);
+        if (Input.GetKey(KeyCode.I) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = (new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            laserLine.SetPosition(0, transform.position += Vector3.right);
+
+            if (!Physics.Raycast(rayOrigin,transform.position ,  out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + ( transform.position * weaponRange));
+            }
+
+        }
     }
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+
+    }
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {

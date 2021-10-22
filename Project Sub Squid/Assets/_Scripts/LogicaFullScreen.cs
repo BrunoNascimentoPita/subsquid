b5918a1b0d6b31593c53f3f71559ea8d549f272b
AndreZrem <80982199+AndreZrem@users.noreply.github.com>
2021-10-22 11:18:08 -0300

    deu merda
    mas vamo ver

--- Project Sub Squid/Assets/_Scripts/LogicaFullScreen.cs
+++ Project Sub Squid/Assets/_Scripts/LogicaFullScreen.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LogicaFullScreen : MonoBehaviour
 {
     public Toggle toggle;
     
+    public TMP_Dropdown resolutionDropDown;
+    Resolution [] resolutions;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,7 @@ public class LogicaFullScreen : MonoBehaviour
            toggle.isOn = false; 
         }
 
+        RevisarResolution();
 
     }
 
@@ -33,4 +38,43 @@ public class LogicaFullScreen : MonoBehaviour
         Screen.fullScreen = fullScreenCompleto;
 
     }
+
+    public void RevisarResolution()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        List<string> options = new List<string>();
+        int resolutionActual = 0;
+
+        for (int i = 0; i > resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                resolutionActual = i;
+
+            }
+
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = resolutionActual;
+        resolutionDropDown.RefreshShownValue();
+
+        resolutionDropDown.value = PlayerPrefs.GetInt("numeroResolution", 0);
+    }
+
+    public void CambiarResolution(int indiceResolution)
+    {
+        PlayerPrefs.SetInt("numeroResolution", resolutionDropDown.value);
+
+        Resolution resolution = resolutions[indiceResolution];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+    }
+
+
+
 }
